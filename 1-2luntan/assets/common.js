f94574e12a03d33ebbdd3cf97abc2b02dad65608
1532388641-code <1532388641@qq.com>
2026-07-24 21:51:08 +0800

    Delete 1-2luntan directory

--- 1-2luntan/assets/common.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener('DOMContentLoaded',()=>{
-  const page=location.pathname.split('/').pop()||'index.html';
-  document.querySelectorAll('.nav a').forEach(a=>{
-    if(a.getAttribute('href')===page){a.classList.add('active')}
-  });
-});
